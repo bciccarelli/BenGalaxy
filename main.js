27b226a93b340f64c363b4b57bc3ce6ae3492608
AluminumChassis2 <32084215+AluminumChassis@users.noreply.github.com>
2018-10-23 20:03:26 -0700

    update with layout + mobile

---
+++ main.js
@@ -0,0 +1,29 @@
+function detectOrientation() {
+	if(window.innerWidth<window.innerHeight) {
+		console.log("less");
+		for (i=0;i<document.getElementsByClassName("project").length;i++) {
+			document.getElementsByClassName("project")[i].style.height = window.innerHeight/6+"px";
+			document.getElementsByClassName("project")[i].style.width = "48%"
+		} 
+	} else {
+		for (i=0;i<document.getElementsByClassName("project").length;i++) {
+			document.getElementsByClassName("project")[i].style.height = 100+"px";
+			document.getElementsByClassName("project")[i].style.width = "15%"
+		} 
+	}
+}
+function toggleMute() {
+
+  var video=document.getElementById("issVid");
+
+  if(video.muted){
+  	document.getElementById("volume").innerText = "Mute";
+    video.muted = false;
+  } else {
+  	document.getElementById("volume").innerText = "Unmute";
+    video.muted = true;
+
+  }
+
+
+}
