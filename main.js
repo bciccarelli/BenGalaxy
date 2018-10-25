6313e1d7b928b27cb8c610babb5c6e5e613492d4
AluminumChassis2 <32084215+AluminumChassis@users.noreply.github.com>
2018-10-25 13:08:40 -0700

    orientation detection done (pretty sure?)

--- main.js
+++ main.js
@@ -12,8 +12,38 @@ function detectOrientation() {
 			console.log("called")
 		} 
 	}
+	if()
 }
-setTimeout(()=>detectOrientation(),100);
+setTimeout(function() {
+	os = getOS();
+	if(os<2) {
+		document.getElementsByTagName("body").removeChild(document.getElementById("issVid"));
+	}
+	detectOrientation();
+},100);
+
+function getOS() {
+  var userAgent = window.navigator.userAgent,
+  platform = window.navigator.platform,
+  macosPlatforms = ['Macintosh', 'MacIntel', 'MacPPC', 'Mac68K'],
+  windowsPlatforms = ['Win32', 'Win64', 'Windows', 'WinCE'],
+  iosPlatforms = ['iPhone', 'iPad', 'iPod'],
+  os = null;
+
+  if (macosPlatforms.indexOf(platform) > -1) {
+    os = 'Mac OS';
+  } else if (iosPlatforms.indexOf(platform) > -1) {
+    os = 0;
+  } else if (windowsPlatforms.indexOf(platform) > -1) {
+    os = 'Windows';
+  } else if (/Android/.test(userAgent)) {
+    os = 1;
+  } else if (!os && /Linux/.test(platform)) {
+    os = 'Linux';
+  }
+
+  return os;
+}
 
 function toggleMute() {
 
