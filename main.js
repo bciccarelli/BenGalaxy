a50f149f0bec87329727e0e543b9ce1179671187
AluminumChassis2 <32084215+AluminumChassis@users.noreply.github.com>
2018-10-25 21:31:38 -0700

    side bar

--- main.js
+++ main.js
@@ -8,7 +8,6 @@ function detectOrientation() {
 		for (i=0;i<document.getElementsByClassName("project").length;i++) {
 			document.getElementsByClassName("project")[i].style.height = 100+"px";
 			//document.getElementsByClassName("project")[i].style.width = "15%";
-			console.log("width")
 		}
 	}
 }
@@ -42,7 +41,27 @@ function getOS() {
 
   return os;
 }
+function sideBarClick() {
+    console.log("click");
+    document.getElementById("sideBar").style.width="95%";
+    document.getElementById("sideBar").disabled=true;
+    document.getElementById("clickAway").style.display="block";
+    console.log(document.getElementById("sideBar"));
+    for (var i = document.getElementsByClassName("t").length - 1; i >= 0; i--) {
+      document.getElementsByClassName("t")[i].style.fontSize="10vh";
+      document.getElementsByClassName("t")[i].style.padding="10px";
+    }
+}
+function clickAway() {
 
+    document.getElementById("sideBar").style.width="4%";
+    document.getElementById("sideBar").disabled=false;
+    document.getElementById("clickAway").style.display="none";
+    for (var i = document.getElementsByClassName("t").length - 1; i >= 0; i--) {
+      document.getElementsByClassName("t")[i].style.fontSize="0vh";
+      document.getElementsByClassName("t")[i].style.padding="0";
+    }
+}
 function toggleMute() {
 
   var video=document.getElementById("issVid");
