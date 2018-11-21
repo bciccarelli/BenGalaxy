085385505a47dfeda495e2c8c94b6b4a078fed36
AluminumChassis2 <32084215+AluminumChassis@users.noreply.github.com>
2018-11-20 19:39:48 -0700

    demo

--- main.js
+++ main.js
@@ -21,7 +21,6 @@ setTimeout(function() {
 },10);
 function pageOnLoad() {
     console.log("page loaded")
-    setupCanvas();
     detectOrientation();
 }
 function getOS() {
@@ -66,91 +65,4 @@ function clickAway() {
       document.getElementsByClassName("sideButton")[i].style.fontSize="0vh";
       document.getElementsByClassName("sideButton")[i].style.padding="0";
     }
-}
-function toggleMute() {
-
-  var video=document.getElementById("issVid");
-
-  if(video.muted){
-  	document.getElementById("volume").innerText = "Mute";
-    video.muted = false;
-  } else {
-  	document.getElementById("volume").innerText = "Unmute";
-    video.muted = true;
-
-  }
-}
-var canvas;
-var ctx;
-var pointRadius = 10;
-var numpoints = 100;
-var points = [];
-var depth=3;
-var d=0;
-var frame = 0
-function setupCanvas() {
-
-    canvas = document.getElementById("bgcanvas");
-    ctx = canvas.getContext("2d");
-    for(var i = 0; i < numpoints; i++) {
-        points[i]=new point();
-    }
-    setInterval(function(){
-     drawSpace();
-    } ,1000/20)
-}
-
-function drawSpace() {
-    frame++;
-    b=Math.floor((Math.sin(frame/40+2*Math.PI/3)+1)*128);
-    r=Math.floor((Math.sin(frame/40+4*Math.PI/3)+1)*128);
-    g=Math.floor((Math.sin(frame/40+2*Math.PI)+1)*128);
-    color="rgb("+r+", "+g+", "+b+")";
-    if (d > 10) {
-      d=0;
-      points.splice(Math.floor(Math.random()*points.length),1);
-      points[points.length]=new point();
-    } else {
-      d++
-    }
-
-    ctx.fillStyle="#000000";
-    ctx.fillRect(0,0,canvas.width,canvas.height);
-    for(var i = 0; i < numpoints; i++) {
-        
-        points[i].draw();
-
-        for(var j = 0; j < numpoints; j ++) {
-            if(distance(points[i],points[j])<150){
-              ctx.strokeStyle=color;
-              ctx.beginPath();
-              ctx.moveTo(points[i].x,points[i].y);
-              ctx.lineTo(points[j].x,points[j].y);
-              ctx.stroke();
-            }
-        }       
-    }
-}
-function threeToTwo(object) {
-    let x=object.x;
-    let y=object.y;
-    x=((x)+object.z*canvas.width/2)/(object.z+1);
-    y=((y)+object.z*canvas.height/2)/(object.z+1);
-    return ({x:x,y:y})
-}
-function distance(obj1,obj2) {
-    return Math.sqrt(Math.pow(obj1.x-obj2.x,2)+Math.pow(obj1.y-obj2.y,2))
-}
-function point() {
-  this.vely=(Math.random()-.5)/10;
-  this.velx=(Math.random()-.5)/10;
-  this.x=Math.random()*canvas.width;
-  this.y=Math.random()*canvas.height;
-  this.draw = function() {
-    this.velx += (Math.random()-.5)/10;
-    this.vely += (Math.random()-.5)/10;
-    this.x+=this.velx;
-    this.y+=this.vely;
-    
-  }
 }
