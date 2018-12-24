fe6d26bd6c6b74b372b08dcb43d70ac770ebdb20
AluminumChassis2 <32084215+AluminumChassis@users.noreply.github.com>
2018-12-23 17:15:12 -0700

    added hero to the top

--- js/widgets.js
+++ js/widgets.js
@@ -1,8 +1,4 @@
-//document.getElementsByTagName('contentRight')[0].style.top=100-document.getElementsByTagName('contentRight')[0].clientHeight+"px";
-function aboutBar() {
-	if(document.getElementsByTagName('contentRight')[0].style.top=="0px"){
-		document.getElementsByTagName('contentRight')[0].style.top=100-document.getElementsByTagName('contentRight')[0].clientHeight+"px";
-	} else {
-		document.getElementsByTagName('contentRight')[0].style.top="0";
-	}
+function goToProjects() {
+	var e = document.getElementById("projects");
+	e.scrollIntoView(true); 
 }
