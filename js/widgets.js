27cd68194e120de6e65e344da248b345f5e9c6be
AluminumChassis2 <32084215+AluminumChassis@users.noreply.github.com>
2018-12-24 16:58:33 -0700

    fixed scrolling bug, hopefully

--- js/widgets.js
+++ js/widgets.js
@@ -1,8 +1,8 @@
 var currentPage=0;
 var maxPage=3;
 var auto = false;
-pageSwitch(0);
 function pageSwitch(num) {
+	console.log(num)
 	currentPage=num;
 	if(currentPage<1){
 		document.getElementById("switchLeft").disabled=true;
@@ -17,18 +17,16 @@ function pageSwitch(num) {
 		document.getElementById("switchRight").disabled=false;
 	}
 	window.scrollTo(screen.width*num,0);
-	setTimeout(function(){auto=false},1500) 
+	setTimeout(function(){auto=false},2000) 
 }
 function right(){
 	if(currentPage<maxPage){
-
 		auto = true;
 		pageSwitch(currentPage+1)	
 	}
 }
 function left(){
 	if(currentPage>0){
-
 		auto = true;
 		pageSwitch(currentPage-1)
 	}
@@ -38,7 +36,12 @@ window.addEventListener("keydown", function(e){
 	if(e.keyCode==37){left();e.preventDefault();}
 	if(e.keyCode==39){right();e.preventDefault();}
 })
+
 var n;
+setInterval(function(){
+	if(!auto){
+		pageSwitch(Math.round(window.scrollX/screen.width))
+	}},1000);
 window.onscroll = function(){
 	var d = new Date();
 	n = d.getTime();
