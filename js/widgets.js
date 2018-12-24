143b2ae7bf80fd614bb63e4af106c0f420dbccd6
AluminumChassis2 <32084215+AluminumChassis@users.noreply.github.com>
2018-12-24 16:09:22 -0700

    horizontal update

--- js/widgets.js
+++ js/widgets.js
@@ -1,4 +1,54 @@
-function goTo(id) {
-	var e = document.getElementById(id);
-	e.scrollIntoView(true); 
+var currentPage=0;
+var maxPage=3;
+var auto = false;
+pageSwitch(0);
+function pageSwitch(num) {
+	currentPage=num;
+	if(currentPage<1){
+		document.getElementById("switchLeft").disabled=true;
+	} else {
+
+		document.getElementById("switchLeft").disabled=false;
+	}
+	if(currentPage>maxPage-1){
+		document.getElementById("switchRight").disabled=true;
+	} else {
+
+		document.getElementById("switchRight").disabled=false;
+	}
+	window.scrollTo(screen.width*num,0);
+	setTimeout(function(){auto=false},1500) 
 }
+function right(){
+	if(currentPage<maxPage){
+
+		auto = true;
+		pageSwitch(currentPage+1)	
+	}
+}
+function left(){
+	if(currentPage>0){
+
+		auto = true;
+		pageSwitch(currentPage-1)
+	}
+}
+
+window.addEventListener("keydown", function(e){
+	if(e.keyCode==37){left();e.preventDefault();}
+	if(e.keyCode==39){right();e.preventDefault();}
+})
+var n;
+window.onscroll = function(){
+	var d = new Date();
+	n = d.getTime();
+	setTimeout(function(){
+		var d = new Date();
+		if(d.getTime()-n>180){
+			console.log("here")
+			if(!auto){
+				pageSwitch(Math.round(window.scrollX/screen.width))
+			}
+		}
+	},200)
+};
